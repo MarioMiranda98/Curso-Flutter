01ae8506bc28684434bf7df19225fc7aaaf29fc0
MarioMiranda98 <farolin95@msn.com>
2020-09-07 18:47:43 -0500

    disenios ok

--- Flutter/disenios/lib/main.dart
+++ Flutter/disenios/lib/main.dart
@@ -1,21 +1,32 @@
-import 'package:disenios/src/pages/scroll_page.dart';
 import 'package:flutter/material.dart';
  
 import 'package:disenios/src/pages/basico_page.dart';
+import 'package:disenios/src/pages/botones_page.dart';
+import 'package:disenios/src/pages/scroll_page.dart';
+import 'package:flutter/services.dart';
+
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.black,
+        statusBarIconBrightness: Brightness.light
+      )
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
 
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico' : (BuildContext context) => BasicoPage(),
         'scroll' : (BuildContext context) => ScrollPage(),
-      },
+        'botones' : (BuildContext context) => BotonesPage(),
+      }, 
     );
   }
 }
